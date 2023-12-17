cebefb40d3634af76344886bda64ba7e4fa18346
Ajith Chakkadath <130172872+Ajith-Chakkadath@users.noreply.github.com>
2023-12-17 18:17:33 +0530

    master

--- src/pages/hero/Hero.jsx
+++ src/pages/hero/Hero.jsx
@@ -6,8 +6,16 @@ export default function Hero() {
     <div className='hmain'>
         <div className="hero">
             <img  src="https://jep-asset.akamaized.net/cms/assets/airfiber/JioAirFiber-Homepage-Desktop1.webp" alt="" />
-            
-            
+            <div className="hcontent">
+              <h4>Intoducing JioFiber</h4>
+        
+              <p>Experience the future of home entertinment with 15+ OTT apps and 550+ digital Tv channels</p>
+           <br></br>
+           <br />
+            <a className='bg-clr-blue'>Know more</a>
+            <a className='br-white'>Book now</a>
+            </div>
+
             </div>
    </div>
   )
